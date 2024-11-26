224a971c4d8a635ae737f2c4e715c5c2ed509cc9
Matheus Couto <158165513+MatheusCouto8@users.noreply.github.com>
2024-11-26 11:10:09 -0300

    feat: criação do mer

--- matheus.sql
+++ matheus.sql
@@ -1,1 +1,22 @@
 -- PÁGINA: CARREIRAS E ESTÁGIOS
+
+--MER
+Entidades:
+- Carreira  
+- Estágio  
+
+Atributos:
+- Carreira:  
+  - id_carreira  
+  - titulo  
+  - descricao  
+
+- Estágio:  
+  - id_estagio  
+  - id_carreira  
+  - descricao  
+
+Relacionamentos:
+Carreira 1:N Estágio  
+- Uma Carreira possui N Estágios.
+- Um Estágio pertence a 1 Carreira.
