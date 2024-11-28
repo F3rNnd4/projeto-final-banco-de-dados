8b762d5dfab7dbe0b9591c44d3a248ef7040bdb3
Pablo Delgado <jessica.p.jesus@aluno.senai.br>
2024-11-28 08:37:32 -0300

    Feat: Criação das Tabelas

--- jessica.sql
+++ jessica.sql
@@ -43,3 +43,35 @@ Dica:
 - Uma Entrevista pertence a 1 Materia.
 - Uma Materia pertence a 1 Entrevistas.
 
+CRIAÇÃO DAS TABELAS:
+
+CREATE TABLE entrevistas (
+    id_entrevista SERIAL PRIMARY KEY,
+    titulo VARCHAR(255),
+    descricao VARCHAR(255),
+    duracao INT,
+    url_video VARCHAR(255)
+);
+
+SELECT * FROM entrevistas;
+
+CREATE TABLE dicas (
+    id_dica SERIAL PRIMARY KEY,
+    texto VARCHAR(255),
+    descricao VARCHAR(255),
+    id_materia INT,
+    FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
+);
+
+SELECT * FROM dicas;
+
+CREATE TABLE materias (
+    id_materia SERIAL PRIMARY KEY,
+    id_entrevista INT,
+    FOREIGN KEY (id_entrevista) REFERENCES entrevistas(id_entrevista)
+);
+
+SELECT * FROM materias;
+
+
+
