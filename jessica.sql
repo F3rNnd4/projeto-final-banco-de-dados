e209398912a01e600dd074a6182f990f76d33117
Pablo Delgado <jessica.p.jesus@aluno.senai.br>
2024-11-28 08:35:59 -0300

    Create: MER

--- jessica.sql
+++ jessica.sql
@@ -1,1 +1,45 @@
--- PÁGINA: ENTREVISTAS E DICAS
+    -- PÁGINA: ENTREVISTAS E DICAS
+    MER
+    Entidades:
+
+- Entrevista
+- Dica
+- Matérias de Entrevistas e Dicas
+
+
+    Atributos:
+
+Entrevista:
+
+- id_entrevista
+
+- titulo
+
+- descrição da entrevista 
+
+- duração (em minutos)
+
+- url_video
+
+Dica:
+
+- id_dica
+
+- texto
+
+- id_materia
+
+
+
+- Relacionamentos:
+
+- Uma Entrevista é única e se relaciona diretamente com Dicas .
+- Uma Dica é única e se relaciona diretamente com Entrevistas.
+
+- Uma Materia de Dica pode ter N Dicas.
+- Uma Dica pertence a 1 Materia.
+
+
+- Uma Entrevista pertence a 1 Materia.
+- Uma Materia pertence a 1 Entrevistas.
+
